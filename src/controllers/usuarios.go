2db6e91c0c871d993261403696cf9161a492d975
leoalvesv <leoalvesvianna@gmail.com>
2024-06-10 20:30:45 -0300

    feat: :zap: finalizar comunicação da controller com o repositório

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -5,6 +5,7 @@ import (
 	"api/src/models"
 	"api/src/repositories"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -28,7 +29,12 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 
 	repositorio := repositories.NovoRepositorioDeUsuarios(db)
-	repositorio.Criar(usuario)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
 }
 
 // BuscarUsuarios buscar todos os usuários salvo no banco de dados
