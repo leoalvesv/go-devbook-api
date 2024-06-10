3eab0656cd5bff93ba139251a0500adf56ce2f75
leoalvesv <leoalvesvianna@gmail.com>
2024-06-10 20:21:41 -0300

    feat: :zap: adicionar repositório de Usuários e função Criar

---
+++ src/repositories/usuarios.go
@@ -0,0 +1,20 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuarios cria um repositório de usuários
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
+}
+
+// Criar insere um usuário no banco de dados
+func (u Usuarios) Criar(usuario models.Usuario) (uint64, error) {
+	return 0, nil
+}
