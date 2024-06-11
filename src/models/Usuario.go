078b7804d2de5f1b4704f13226870b64cdfffccd
leoalvesv <leoalvesvianna@gmail.com>
2024-06-11 12:09:27 -0300

    feat: :zap: criar validação na model de usuario

--- src/models/Usuario.go
+++ src/models/Usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
@@ -9,4 +13,40 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.Validar(); erro != nil {
+		return erro
+	}
+
+	usuario.Formatar()
+	return nil
+}
+
+func (usuario *Usuario) Validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) Formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
